5512971e20f2fc4b27a5f5f1121a6f7dd951eb6d
satvik <satvik.jagannath7@gmail.com>
2014-05-11 05:11:15 +0530

    port dix

--- launch.py
+++ launch.py
@@ -82,4 +82,5 @@ def answer_url():
     return Response(str(response), mimetype='text/xml')
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0',port=port)
